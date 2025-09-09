06c6e277d02e4b4b1406e86b9f7a28a354bd32a2
JuleBehrens <jule_behrens@yahoo.de>
2025-09-09 17:21:22 +0200

    Opus decoding framesPerBuffer*2 instead of samples

--- source/transceiver/audiomanager.cpp
+++ source/transceiver/audiomanager.cpp
@@ -365,7 +365,7 @@ spAudioData_t AudioManager::DecodeWithOpus(const spAudioData_t &spReflectedAudio
         qWarning() << "Audiomanager: Opus encountered an error while decoding data.";
     }
 
-    // CHECK: Not sure if "samples" is the size needed here
+    // CHECK: Not sure if "samples" is the size needed here, should samples be equal to framesPerBuffer?
     auto spDecodedData = std::make_shared<QByteArray>(reinterpret_cast<const char *> (decodedAudioData), m->framesPerBuffer*2);
 
     opus_custom_decoder_ctl(m->decoder, OPUS_RESET_STATE);
